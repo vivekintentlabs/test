1236fd9b5d2de6f5a41241f68ef4c6db273f6d1b
Vivek Pandya <vivekpandya.ce@gmail.com>
2021-11-11 18:44:31 +0530

    Test

---
+++ src/clipboard.d.ts
@@ -0,0 +1,23 @@
+/**
+ * Extending clipboard functionality to include non-finalized read/write
+ * feature, which can be used for images. See:
+ * https://github.com/Microsoft/TypeScript/issues/26728#issuecomment-788076992
+ */
+interface Clipboard {
+  read?(): Promise<Array<ClipboardItem>>
+  write?(items: Array<ClipboardItem>): Promise<void>
+}
+
+interface ClipboardItem {
+  readonly types: string[]
+  getType: (type: string) => Promise<Blob>
+}
+
+declare var ClipboardItem: {
+  prototype: ClipboardItem
+  new(objects: Record<string, Blob>): ClipboardItem
+}
+declare var ClipboardItem: {
+  prototype: ClipboardItem
+  new(objects: Record<string, Blob>): ClipboardItem
+}
